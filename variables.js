20f5b2309fdf6e24319b57d64c6bba5a81e1475c
Generosa Litton <glcodeworks@gmail.com>
2024-05-22 17:16:21 -0500

    variable scope exercises

---
+++ variables.js
@@ -0,0 +1,31 @@
+// let color = "yellow";
+// let colors = ["red", "green", "blue"];
+
+// function updateColors(colors) {
+//   colors.push(color);
+// }
+
+// updateColors(colors);
+// console.log(colors);
+
+// let color = "yellow";
+// let colors = ["red", "green", "blue"];
+
+// function updateColors(colors, color) {
+//   colors.push(color);
+// }
+
+// updateColors(colors);
+// console.log(colors);
+
+let color1 = "purple";
+let color2 = "pink";
+let colors = ["red", "green", "blue"];
+
+function updateColors(colors, color) {
+  colors.push(color);
+}
+
+updateColors(colors, color1);
+updateColors(colors, color2);
+console.log(colors);
